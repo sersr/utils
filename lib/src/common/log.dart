e96bc48b17ad74ca3fbab0a681d257953433e6de
sersr <1334349436@qq.com>
2021-11-25 00:41:28 +0800

    log: 限制每一行的字数

--- lib/src/common/log.dart
+++ lib/src/common/log.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
+import 'package:characters/characters.dart';
 import 'dart:math' as math;
 
 const bool releaseMode =
@@ -16,7 +16,7 @@ abstract class Log {
   static const int warn = 1;
   static const int error = 2;
   static int level = 0;
-  static int functionLength = 24;
+  static int functionLength = 18;
 
   static bool i(Object? info,
       {bool showPath = true, bool onlyDebug = true, Zone? zone}) {
@@ -48,7 +48,8 @@ abstract class Log {
       [Zone? zone]) {
     if (message == null || (!debugMode && onlyDebug)) return true;
     zone ??= Zone.current;
-    var addMsg = '';
+    var start = '';
+    var end = '';
 
     var path = '', name = '';
 
@@ -70,37 +71,91 @@ abstract class Log {
       }
     }
 
-    addMsg = '$addMsg$name|$message.';
+    start = '$start$name|';
 
     if (!Platform.isIOS) {
-      var start = '';
+      var s = '';
       switch (lv) {
         case info:
-          start = '\x1B[39m';
+          s = '\x1B[39m';
           break;
         case warn:
-          start = '\x1B[33m';
+          s = '\x1B[33m';
           break;
         case error:
-          start = '\x1B[31m';
+          s = '\x1B[31m';
           break;
         default:
-          start = '';
+          s = '';
       }
-      addMsg = '$start$addMsg\x1B[0m';
+      start = '$s$start';
+      end = '\x1B[0m';
     }
 
     if (showPath) {
       if (debugMode) {
-        addMsg = '$addMsg $path';
+        end = '$end $path';
       } else {
         var _path = path.replaceAll(')', '');
-        addMsg = '$addMsg $_path:1)';
+        end = '$end $_path:1)';
       }
     }
+    final split = '$message'.split('\n').expand((e) => splitString(e)).toList();
 
-    // ignore: avoid_print
-    zone.print(addMsg);
+    for (var i = 0; i < split.length; i++) {
+      if (i < split.length - 1) {
+        zone.print('$start${split[i]}');
+      } else {
+        zone.print('$start${split[i]}$end');
+      }
+    }
     return true;
+  }
+
+  static List<String> splitString(Object source) {
+    final rawSource = source.toString().characters;
+    final length = rawSource.length;
+    final list = <String>[];
+    if (length == 0) return list;
+    const maxLength = 110;
+    for (var i = 0; i < length;) {
+      final end = math.min(i + 84, length);
+      final subC = rawSource.getRange(i, end);
+      final sub = subC.toString();
+      var count = 0;
+      if (sub.length > maxLength / 2) {
+        for (var element in sub.codeUnits) {
+          if (element >= 19968 && element <= 40869) count++;
+          if (count > 5) {
+            break;
+          }
+        }
+      }
+      if (count <= 5) {
+        list.add(sub);
+        i = end;
+      } else {
+        final buffer = StringBuffer();
+        var hasLenght = maxLength;
+        for (var item in subC) {
+          if (hasLenght <= 0) break;
+          buffer.write(item);
+          if (item.length > 1) {
+            hasLenght -= 2;
+            continue;
+          }
+          final itemCode = item.codeUnits.first;
+          if (itemCode >= 19968 && itemCode <= 40869) {
+            hasLenght -= 2;
+            continue;
+          }
+          hasLenght -= 1;
+        }
+        final source = buffer.toString();
+        i += source.characters.length;
+        list.add(source);
+      }
+    }
+    return list;
   }
 }
